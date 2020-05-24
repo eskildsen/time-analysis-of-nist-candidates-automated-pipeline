1bd930291314753c0959c3b40e931577d4394b1d
ADAM-PC\Adam <adamblatchley@hotmail.com>
2020-05-24 18:02:55 +0200

    function calls working

--- run.ps1
+++ run.ps1
@@ -1,7 +1,7 @@
 # RED='\033[0;31m'
 # NC='\033[0m'
 
-function help () {
+function help {
     Write-Output "Usage:"
     Write-Output "Run using prebuild docker image:"
     Write-Output "   ./run.sh source_dir out_dir"
@@ -10,26 +10,25 @@ function help () {
     exit 0
 }
 
-function check_path () {
-    local path=$1
-    local number=$2
-    if (-not (Test-Path -Path $path -PathType Container )) { Write-Output "${RED}The ${number} argument is not a directory${NC}"; help }
-    if (-not ([System.IO.Path]::IsPathRooted($path))) {Write-Output "${RED}The ${number} argument is not an absolute path${NC}"; help }
+function check_path {
+    param ([string]$path, [string]$number)
+    if (-not (Test-Path -Path $path -PathType Container )) { Write-Output "${RED}The $number argument is not a directory${NC}"; help }
+    if (-not ([System.IO.Path]::IsPathRooted($path))) {Write-Output "${RED}The $number argument is not an absolute path${NC}"; help }
 }
 
 # if (-not [ ! -x "$(command -v docker)" ]; Write-Output "${RED}Could not find docker, make sure that you have docker installed${NC}" && exit 0
 
-if (("$1" -eq "-b") -and ($args.Count -eq  3)) {
-    check_path $2 "second"
-    check_path $3 "third"
+if (("$args[1]" -eq "-b") -and ($args.Count -eq  3)) {
+    check_path $args[2] "second"
+    check_path $args[3] "third"
 
     has_tools=$(docker images | grep time-analysis-tools | wc -l)
     if ($has_tools -eq 0) { docker build -t time-analysis-tools -f Dockerfile .}
     docker run --rm -it -v ${2}:/root/source -v ${3}:/root/out time-analysis-tools
 }
 elseif ($args.Count -eq 2 ){
-    check_path $1 "first"
-    check_path $2 "second"
+    check_path $args[0] "first"
+    check_path $args[1] "second"
     docker run --rm -it -v ${1}:/root/source -v ${2}:/root/out eskehoy/ct-analysis-tools:latest
 }
 else {help}
